c954d3daf8d4cebc66ed9e44bb1b1095ff471131
FeatFeat <freedy.ebah@yahoo.com>
2025-10-29 03:11:44 -0400

    Exercie1

---
+++ Exercice2.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+void main() {
+  try {
+    print("Saisis ton âge : ");
+    String? input = stdin.readLineSync();
+    int age = int.parse(input ?? "0");
+    print("Ton âge est $age ans.");
+  } catch (e) {
+    print("Erreur : Veuillez entrer un nombre valide.");
+  } finally {
+    print("Fin d'opération.");
+  }
+}
